11925df10b956a6461488747ffd001d2221b4fbc
Dnny <mcmillandanny@gmail.com>
2019-01-19 17:22:20 -0500

    working on loader screen

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,2 +1,19 @@
-"use strict";console.log("Hello World from main.js! \nChange this message, and make sure it changes in the browser \nto verify that you're working in the right files.");
+"use strict";
+
+console.log("Shaky Knees");
+
+//Speaker Loader Animations
+var loaderSpeaker = new TimelineMax({ delay: 1.5 });
+
+loaderSpeaker.from(".speaker-loader", .4, { y: "-1000px" });
+loaderSpeaker.to(".speaker-loader", 2, { y: "0px" });
+// loaderSpeaker.to(".speaker-loader", 1, {opacity: 1});
+
+
+//Logo Loader Animations
+var loaderLogo = new TimelineMax({ delay: 0.4 });
+
+loaderLogo.from(".logo-loader", 1, { opacity: 0 });
+loaderLogo.to(".logo-loader", 2, { opacity: 0.5 });
+loaderLogo.to(".logo-loader", 2, { opacity: 1 });
 //# sourceMappingURL=main.js.map
