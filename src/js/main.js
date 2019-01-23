e6c9a820fb554844bc8f036987d88d47d306c236
Dnny <mcmillandanny@gmail.com>
2019-01-23 11:15:55 -0500

    fixed speaker

--- src/js/main.js
+++ src/js/main.js
@@ -4,8 +4,7 @@ var loaderSpeaker = new TimelineMax({delay: 1.5});
 
 loaderSpeaker.from(".speaker-loader", 2, {y: "-1200px", opacity: 1}) 
 loaderSpeaker.to(".speaker-loader", 1, { y: "0", ease: CustomEase.create("custom", "M0,0 C0.323,0 0.38,0.023 0.433,0.05 0.554,0.11 0.764,0.23 0.805,0.44 0.849,0.668 0.839,0.734 0.932,0.734 0.999,0.734 0.974,0.656 1,0.632")
-})
-loaderSpeaker.to(".speaker-loader", 1, {opacity: 1});
+});
 
 
 //Logo Loader Animations
