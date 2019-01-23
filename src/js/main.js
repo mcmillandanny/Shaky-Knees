1c49bb6e7995181709ebc6ec4c7f62a04332d197
Dnny <mcmillandanny@gmail.com>
2019-01-23 10:54:30 -0500

    adjusted loading screen

--- src/js/main.js
+++ src/js/main.js
@@ -2,16 +2,16 @@ console.log("Shaky Knees");
 //Speaker Loader Animations
 var loaderSpeaker = new TimelineMax({delay: 1.5});
 
-loaderSpeaker.from(".speaker-loader", .3, {y: "-1000px"}) 
-loaderSpeaker.to(".speaker-loader", .5, { y: "0", ease: CustomEase.create("custom", "M0,0 C0.323,0 0.38,0.023 0.433,0.05 0.554,0.11 0.764,0.23 0.805,0.44 0.849,0.668 0.839,0.734 0.932,0.734 0.999,0.734 0.974,0.656 1,0.632")
-});
+loaderSpeaker.from(".speaker-loader", 2, {y: "-1200px", opacity: 1}) 
+loaderSpeaker.to(".speaker-loader", 1, { y: "0", ease: CustomEase.create("custom", "M0,0 C0.323,0 0.38,0.023 0.433,0.05 0.554,0.11 0.764,0.23 0.805,0.44 0.849,0.668 0.839,0.734 0.932,0.734 0.999,0.734 0.974,0.656 1,0.632")
+})
 loaderSpeaker.to(".speaker-loader", 1, {opacity: 1});
 
 
 //Logo Loader Animations
 var loaderLogo = new TimelineMax({delay: 0.4});
 
-loaderLogo.from(".logo-loader",1, {opacity: 0})
-loaderLogo.to(".logo-loader",1, {opacity: 0.5})
+loaderLogo.from(".logo-loader",1, {opacity: 0, ease:Back.easeIn})
+// loaderLogo.to(".logo-loader",1, {opacity: 0.5})
 loaderLogo.to(".logo-loader",1, {opacity: 1})
-loaderLogo.to(".loader", 1, {display: "none"});
+loaderLogo.to(".loader", 4, {display: "none"});
