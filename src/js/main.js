cd36ae616f40092f4ed000b9dc5e969e02b40478
Dnny <mcmillandanny@gmail.com>
2019-01-26 17:36:39 -0500

    coding out mobile

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,48 @@
 console.log("Shaky Knees");
+
+
 //Speaker Loader Animations
-var loaderSpeaker = new TimelineMax({delay: 1.5});
+// var loaderSpeaker = new TimelineMax({delay: 1.5});
 
-loaderSpeaker.from(".speaker-loader", 2, {
-    y: "-1200px", 
-    opacity: 1,
-    ease: CustomEase.create("custom", "M0,0,C0.14,0,0.719,0.981,0.726,0.998,0.788,0.914,0.84,0.936,0.859,0.95,0.878,0.964,0.897,0.985,0.911,0.998,0.922,0.994,0.939,0.984,0.954,0.984,0.969,0.984,1,1,1,1")
-});
+// loaderSpeaker.from(".speaker-loader", 2, {
+//     y: "-1200px", 
+//     opacity: 1,
+//     ease: CustomEase.create("custom", "M0,0,C0.14,0,0.719,0.981,0.726,0.998,0.788,0.914,0.84,0.936,0.859,0.95,0.878,0.964,0.897,0.985,0.911,0.998,0.922,0.994,0.939,0.984,0.954,0.984,0.969,0.984,1,1,1,1")
+// });
 
-//Logo Loader Animations
+// //Logo Loader Animations
 var loaderLogo = new TimelineMax({delay: 0.4});
 
-loaderLogo.from(".logo-loader",1, {
-    opacity: 0, 
-    ease:Back.easeIn
+// loaderLogo.from(".logo-loader",1, {
+//     opacity: 0, 
+//     ease:Back.easeIn
+// })
+// loaderLogo.to(".logo-loader",1, {
+//     opacity: 1
+// })
+loaderLogo.to(".loader", 1, {
+    display: "none"
+});
+
+
+/// Skull Load in animation
+var skull = new TimelineMax({dely: 0.6});
+
+skull.to(".skull1", 2 , {
+    x: "1150px"
 })
-loaderLogo.to(".logo-loader",1, {
-    opacity: 1
+
+
+skull.to(".skull2", 2 , {
+    x: "1150px"
+
 })
-// loaderLogo.to(".loader", 4, {
-//     display: "none"
-// });
+
+skull.to(".skull1", .5, {
+    display: "none"
+})
+
+skull.to(".skull2", 3, {
+    display: "block",
+    rotaion: 20
+})
