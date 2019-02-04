d83e75d97de3e66e8ccfef134e4f7e7f791ea31c
Dnny <mcmillandanny@gmail.com>
2019-02-04 13:47:22 -0500

    adding styles

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,57 +2,69 @@
 
 console.log("Shaky Knees");
 
-var splitTextedStuff = new SplitText(".band-names p", { type: 'words' });
+// var splitTextedStuff = new SplitText(".band-names p", {type: 'words'});
+// var year = new  SplitText('.year', {type: 'chars'});
 
-// TweenMax.staggerFrom(splitTextedStuff.words, 1, {scale: 0, left: -320, top: -110 }, 0.5)
+// // TweenMax.staggerFrom(splitTextedStuff.words, 1, {scale: 0, left: -320, top: -110 }, 0.5)
 
+// year.chars.forEach(function(letter, i) {
 
-splitTextedStuff.words.forEach(function (word, i) {
-
-    TweenMax.from(word, 1, {
-        delay: 2 + i * 0.1,
-        scale: 0,
-        left: -word.offsetLeft + 110,
-        top: -word.offsetTop + 120
-    });
-});
-//Speaker Loader Animations
-// var loaderSpeaker = new TimelineMax({delay: 1.5});
-
-// loaderSpeaker.from(".speaker-loader", 2, {
-//     y: "-1200px", 
-//     opacity: 1,
-//     ease: CustomEase.create("custom", "M0,0,C0.14,0,0.719,0.981,0.726,0.998,0.788,0.914,0.84,0.936,0.859,0.95,0.878,0.964,0.897,0.985,0.911,0.998,0.922,0.994,0.939,0.984,0.954,0.984,0.969,0.984,1,1,1,1")
-// });
-
-// //Logo Loader Animations
-var loaderLogo = new TimelineMax({ delay: 0.4 });
-
-loaderLogo.from(".logo-loader", 1, {
-    opacity: 0,
-    ease: Back.easeIn
-});
-loaderLogo.to(".logo-loader", 1, {
-    opacity: 1
-});
-// loaderLogo.to(".loader", 1, {
-//     display: "none"
+//         TweenMax.from(letter, 1, {
+//             scale: 1.4
+//         })
 // });
 
 
-/// Skull Load in animation
-var skull = new TimelineMax({ dely: 0.6 });
+// splitTextedStuff.words.forEach(function(word, i) {
 
-skull.to(".skull1", 2, {
-    x: "1150px"
-});
+//     TweenMax.from(word, 1, {
+//         delay: 2 + (i * 0.1), 
+//         scale: 0, 
+//         left: -word.offsetLeft + 110, 
+//         top: -word.offsetTop +  120
+//     })
+// })
+// //Speaker Loader Animations
+// // var loaderSpeaker = new TimelineMax({delay: 1.5});
 
-skull.to(".skull2", 2, {
-    x: "1150px"
+// // loaderSpeaker.from(".speaker-loader", 2, {
+// //     y: "-1200px", 
+// //     opacity: 1,
+// //     ease: CustomEase.create("custom", "M0,0,C0.14,0,0.719,0.981,0.726,0.998,0.788,0.914,0.84,0.936,0.859,0.95,0.878,0.964,0.897,0.985,0.911,0.998,0.922,0.994,0.939,0.984,0.954,0.984,0.969,0.984,1,1,1,1")
+// // });
 
-});
+// // //Logo Loader Animations
+// var loaderLogo = new TimelineMax({delay: 0.4});
 
-skull.to(".skull2", 3, {
-    rotaion: 20
-});
+// loaderLogo.from(".logo-loader",1, {
+//     opacity: 0, 
+//     ease:Back.easeIn
+// })
+// loaderLogo.to(".logo-loader",1, {
+//     opacity: 1
+// })
+// // loaderLogo.to(".loader", 1, {
+// //     display: "none"
+// // });
+
+
+// /// Skull Load in animation
+// // var skull = new TimelineMax({dely: 0.6});
+
+// // skull.to(".skull1", 2 , {
+// //     x: "1150px"
+// // })
+
+
+// // skull.to(".skull2", 2 , {
+// //     x: "1150px"
+
+// // })
+
+// // skull.to(".skull2", 3, {
+// //     rotaion: 20
+// // })
+
+
+// TweenMax.to('.page1svg #INCUBUS', 5, {rotate: '720deg'})
 //# sourceMappingURL=main.js.map
